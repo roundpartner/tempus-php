6baa5a8d45d2b24dd8251d7f59412759382aa40f
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-12-22 02:19:15 +0000

    Validates scenario

--- src/Tempus.php
+++ src/Tempus.php
@@ -40,14 +40,18 @@ class Tempus extends RestClient implements TempusInterface
     /**
      * @param string $token
      * @param int $userId
+     * @param string $scenario
      *
      * @return Token
      *
      * @throws \Exception
      */
-    public function validate($token, $userId)
+    public function validate($token, $userId, $scenario = null)
     {
         $uri = sprintf('/%d/%s', $userId, $token);
+        if (null !== $scenario) {
+            $uri = sprintf('/%d/%s/%s', $userId, $scenario, $token);
+        }
         try {
             $response = $this->client->get($uri);
         } catch (ClientException $exception) {
